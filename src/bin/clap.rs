c2c27ac93f2281cce1909943ff1f52c2200aca09
alphonsezhang <lovestudy@qq.com>
2024-07-11 10:11:40 +0800

    clap over

--- src/bin/clap.rs
+++ src/bin/clap.rs
@@ -1,22 +1,63 @@
-use clap::Parser;
+use std::path::PathBuf;
 
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
 #[command(version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+    // 使用计数动作（clap::ArgAction::Count），意味着每多指定一次 -d 或 --debug，它的值就增加 1。
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+ // Commands 枚举定义了子命令 Test，它本身可以接受一个布尔类型的参数 list。
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
 }
 
 fn main() {
-    let args = Args::parse();
+    let cli = Cli::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
+    // 检查了 name 和 config 参数是否被提供
+    if let Some(name) = cli.name.as_deref() {
+        println!("Value for name: {name}");
+    }
+
+    if let Some(config_path) = cli.config.as_deref() {
+        println!("Value for config: {}", config_path.display());
+    }
+
+    // debug 参数被指定的次数来判断调试模式的状态。
+    match cli.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    // 检查是否存在子命令 Test，并根据 list 参数的值来决定是否打印测试列表
+    match &cli.command {
+        Some(Commands::Test { list }) => {
+            if *list {
+                println!("Printing testing lists...");
+            } else {
+                println!("Not printing testing lists...");
+            }
+        }
+        None => {}
     }
 }
