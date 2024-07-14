e2d4a1868001a5ff3ded4a5e2332b4d9a8d36b12
alphonsezhang <lovestudy@qq.com>
2024-07-14 11:30:08 +0800

    添加修改

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,4 @@
+use clap::Args;
 use clap::Subcommand;
 use clap::Parser;
 // 通过.cargo中config.toml中配置[alias]中
@@ -39,3 +40,40 @@ enum Commands {
     Service,
 }
 
+// 用于映射参数的结构体
+#[derive(Args, Default)]
+struct InferenceArgs {
+    /// Model directory.
+    #[clap(short, long)]
+    model: String,
+    /// Model type, maybe "llama", "mixtral", "llama" by default.
+    #[clap(long)]
+    model_type: Option<String>,
+
+    /// Log level, may be "off", "trace", "debug", "info" or "error".
+    #[clap(long)]
+    log: Option<String>,
+
+    /// Random sample temperature.
+    #[clap(long)]
+    temperature: Option<f32>,
+    /// Random sample top-k.
+    #[clap(long)]
+    top_k: Option<usize>,
+    /// Random sample top-p.
+    #[clap(long)]
+    top_p: Option<f32>,
+
+    /// Select turbo hardware, the format is "ty:detail".
+    #[clap(long)]
+    turbo: Option<String>,
+}
+
+#[derive(PartialEq)]
+enum ModelType {
+    Llama,
+    Mixtral,
+}
+
+impl InferenceArgs {
+}
